6959005d6cf479605dafd1c3d6c6ca5821194ad8
Yuliya <ju7iab@gmail.com>
2023-01-23 20:20:53 +0300

    Contact addition

--- HomeWork.py
+++ HomeWork.py
@@ -4,23 +4,37 @@ def writeFile(file_name):
     with open(file_name, 'a') as data:
         data.writelines("Hello world111" + '\n')
 
-
 def readFile(file_name):
     result = []
     with open(file_name, 'r+') as data:
         for line in data:
-            result.append(line.split())
+            result.append(line.split(','))
     return result
 
-
-def findUsers(userList):
-    name = 'Ivan,'
+def findName(userList):
+    name = input('Введите имя, начиная с пробела: ')
     for user in userList:
         if user[1] == name:
             print(user[3])
+    else:
+        print('Запрашиваемое имя не найдено. Попробуйте ещё раз.')
 
+findName(readFile(fileName))
 
-# writeFile(fileName)
-print(type(readFile(fileName)))
-print(readFile(fileName))
-findUsers(readFile(fileName))
+def newPer(file_name):
+    with open(file_name, 'a') as data:
+        create = input('Введите ФИО и номер телефона через запятую: ')
+        data.writelines(create)
+
+def findTel(userList):
+    tel = input('Введите номер телефона, начиная с пробела и "+": ')
+    for user in userList:
+        if user[3] == tel:
+            print(user[0], user[1], user[2])
+    else:
+        print('Запрашиваемый номер не найден. Создать новый контакт?')
+        create = input('Введите "да" или "нет": ')
+        if create == 'да':
+            print(newPer(fileName))
+
+findTel(readFile(fileName))
